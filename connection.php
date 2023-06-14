cac8bfbb2ca5e845722193c65a14cd1569fa11cf
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-14 17:23:26 +0700

    test

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
- $conn = mysqli_connect('localhost', 'root', '', 'online_shopping')
+ $conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b9449b761b7f52', '61e72d88', 'heroku_4a04c8bc447b15c')
         or die("can not connect database".mysqli_connect());
 
 ?>
