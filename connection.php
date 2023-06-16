e263d5f5ed141024cd97ffccc6ddb894e12d93f1
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-16 16:42:19 +0700

    tesy

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
- $conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b005b084ee5df9', 'c1fadf91', 'heroku_b3a82fe5dfeba2b')
+ $conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b884fa8bf3dc7b', '01aba9aa', 'heroku_6551f465d8af366')
         or die("can not connect database".mysqli_connect());
 
 ?>
