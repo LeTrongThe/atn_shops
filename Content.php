2f1ae75c4eb27d39db4e7eaa4fb10e68dac59ffa
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-22 22:21:31 +0700

    231

--- Content.php
+++ Content.php
@@ -3,49 +3,16 @@
 <?php
 include_once("connection.php");
 ?>
-   
-  
- <!DOCTYPE html>
-<html>
-<head>
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
 
-.price {
-  color: grey;
-  font-size: 22px;
-}
+    
 
-.card button {
-  border: none;
-  outline: 0;
-  padding: 12px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
+ <div class="section trending">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+        </div>
 
-.card button:hover {
-  opacity: 0.7;
-}
-
-
-</style>
-</head>
-<body>
-
-<h2 style="text-align:center">Product Card</h2>
-
-<?php
+        <?php
 						  // 	include_once("database.php");
 		  				   	$result = mysqli_query($conn, "SELECT * FROM product" );
 			
@@ -56,20 +23,83 @@ include_once("connection.php");
 			            
 			                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 				            ?>
+                            
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="product-imgs/<?php echo $row['Pro_image']?>" width="306" height="306"></a>
+              <span class="price"><?php echo  $row['Price']?>$</span>
+            </div>
+            <div class="down-content">
+              <span class="category">Name</span>
+              <h4><?php echo  $row['Product_Name']?></h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
 
-<div class="card">
-<img src="product-imgs/<?php echo $row['Pro_image']?>" width="306" height="306">
-  <h1><?php echo  $row['Product_Name']?></h1>
-  <p class="price"><?php echo  $row['Price']?>$</p>
-  <p><button>Add to Cart</button></p>
-</div>
-
-<?php
+        <?php
                 
-              }
-              ?>
+            }
+            ?>
+ 
 
-</body>
-</html>
+      </div>
+    </div>
+  </div>
+   
+  <div class="maincontent-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-product">
+                        <h2 class="section-title">Product</h2>
+                        <div class="product-carousel">
+                        
+                        <!--Load san pham tu DB -->
+                           <?php
+						  // 	include_once("database.php");
+		  				   	$result = mysqli_query($conn, "SELECT * FROM product" );
+			
+			                if (!$result) { //add this check.
+                                die('Invalid query: ' . mysqli_error($conn));
+                            }			            
+			                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+				            ?>
+                            
+				            <!--Một sản phẩm -->
+                            
+                
+                    <div class="col-md-4 pb-2" id="itemss" >
+                    <div class="card">
+                        <img style="height:300px; margin-top: 50px;"             
+                        src="img/<?=$row['Pro_image']?>"
+                        class="card-img-top"
+                        alt="Product" style="margin: auto;
+                        width: 300px;"
+                        />
+                        
+                        <div class="card-body">
+                        <a href="?page=detail&&id=<?php echo $row["Product_ID"];?>" class="text-decoration-none">
+                        <h5 class="card-title"><?=$row['Product_Name']?></h5></a>
 
+                        <h6 class="card-subtitle mb-2 text-muted"><span></span><?=$row['Price']?>$</h6>
 
+                        <a href="?page=cart&&id=<?php echo $row["Product_ID"];?>" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                        
+                        </div>
+                    </div>
+                <?php
+                
+                }
+				?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End main content area -->
+                        
