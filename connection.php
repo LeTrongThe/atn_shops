f1dea2a98b47453affb34b6e2a7281981f478657
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-23 14:58:18 +0700

    upp

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','bd833d0107e45d','6102c09e','heroku_aaba8fc04b9852d') 
+    $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b44468edeb7d17','292ebb02','heroku_687437c2f670162') 
     or die("Cannot connect database".mysqli_connect_error());
 
     // $conn = mysqli_connect("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","im6uv6tb0uzh0kef","b4ihu2lkxh8ho1b2","mhzmhjrrq860l1d5") 
