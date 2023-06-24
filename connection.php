8e30518663cd1b0b4780c0d33afd0036c0ba85f4
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-25 00:50:57 +0700

    uppp

--- connection.php
+++ connection.php
@@ -1,9 +1,7 @@
 <?php 
-    $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b44468edeb7d17','292ebb02','heroku_687437c2f670162') 
+    $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b40c06663c95b9','e9ab330c','heroku_687437c2f670162') 
     or die("Cannot connect database".mysqli_connect_error());
 
-    // $conn = mysqli_connect("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","im6uv6tb0uzh0kef","b4ihu2lkxh8ho1b2","mhzmhjrrq860l1d5") 
-    // or die("Cannot connect database".mysqli_connect_error());
 
     // mysqli_query($conn, 'SET NAMES "utf8"')
 ?>
